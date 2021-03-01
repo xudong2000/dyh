4e3d7ed65b3493547c7b5a72fa0f76b63b88e6bf
coderwxd <1691083073@qq.com>
2021-03-01 21:00:35 +0800

    ok

--- src/views/class/Class.vue
+++ src/views/class/Class.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="class">
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
     <el-table :data="tableData" style="width: 100%" max-height="250">
       <el-table-column fixed prop="date" label="日期" width="150">
       </el-table-column>
@@ -103,5 +108,20 @@ export default {
   width: 100%;
   height: 100%;
   background-color: #fff;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
